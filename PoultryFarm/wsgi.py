6cefd184c6337373e14f6949dd86aac704feaefa
Turyatemba Silaj <Turyatemba-silaj@users.noreply.github.com>
2026-07-13 13:10:12 +0300

    Bootstrap Vercel admin user

--- PoultryFarm/wsgi.py
+++ PoultryFarm/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
+from django.db import OperationalError, ProgrammingError
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PoultryFarm.settings')
 
@@ -20,3 +22,32 @@ application = get_wsgi_application()
 if settings.IS_VERCEL and not os.environ.get('DATABASE_URL'):
     call_command('migrate', interactive=False, verbosity=0)
 
+
+def bootstrap_superuser():
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+
+    if not username or not password:
+        return
+
+    User = get_user_model()
+    user, _ = User.objects.get_or_create(
+        username=username,
+        defaults={
+            'email': email,
+            'is_staff': True,
+            'is_superuser': True,
+        },
+    )
+    user.email = email or user.email
+    user.is_staff = True
+    user.is_superuser = True
+    user.set_password(password)
+    user.save()
+
+
+try:
+    bootstrap_superuser()
+except (OperationalError, ProgrammingError):
+    pass
